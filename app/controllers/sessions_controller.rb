c62969fc79cb097e7fa21973d38a8ae6da90e3d6
nicolewang2000 <nicole.wang@yale.edu>
2020-06-29 16:23:33 -0400

    add user validations, started form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,22 @@ class SessionsController < ApplicationController
     end
 
     def create
+        # @user = User.find_by(username: params[:username])
+        # if @user && @user.authenticate(params[:password])
+        #     session[:user_id] = @user.id
+        #     redirect_to user_path(@user)
+        # else
+        #     flash[:danger] = "Improper credentials given"
+        #     render :new
+        # end
     end
 
     def destroy
-        # session.delete :username
-        # redirect_to controller: 'application', action: 'hello'
+    #     session.clear
+    #     redirect_to '/'
     end
+
+    def create
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ApplicationRecord
-  #has_secure_password
+  has_secure_password
   has_many :club_memberships
   has_many :clubs, through: :club_memberships
   has_many :books, through: :clubs
-  validates :username, uniqueness: true
 
+  validates :username, uniqueness: true
+  validates :username, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  
   def full_name
-    self.first_name + " " + self.last_name 
+    (self.first_name + " " + self.last_name).titleize
   end
   
 end
